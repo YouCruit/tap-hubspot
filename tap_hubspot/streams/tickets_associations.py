b6867508ed3dc6e396e41d8a1e3482d5d806ab99
Tony <tony.prokopenko@gmail.com>
2023-09-08 17:54:19 +0300

    [BUGFIX] Lint and string fix

--- tap_hubspot/streams/tickets_associations.py
+++ tap_hubspot/streams/tickets_associations.py
@@ -12,8 +12,8 @@ class TicketsAssociationsStream(HubSpotStream):
     request_limit = 50
 
     name = "tickets_associations"
-    path = """/crm/v4/objects/tickets/?associations=companies,contacts
-    &propertiesWithHistory=hubspot_owner_id"""
+    path = "/crm/v4/objects/tickets/?associations=companies,contacts" \
+           "&propertiesWithHistory=hubspot_owner_id"
     properties_object_type = "tickets"
     primary_keys = ["id"]
     replication_key = None
